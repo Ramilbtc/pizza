862e675ab71d27be58288dee581f23559e511c9b
Ramil <105861531+Ramilbtc@users.noreply.github.com>
2023-02-27 20:38:16 +0300

    7.1

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import React from 'react';
 import Header from './components/Header';
 import Sort from './components/Sort';
 import Categories from './components/Categories';
-import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 import './scss/app.scss';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://63fc3ef6859df29986b8ebfe.mockapi.io/items')
       .then((res) => res.json())
       .then((data) => {
-        setItems(data);
+        setTimeout(() => {
+          setItems(data);
+          setIsLoading(false);
+        }, 1000);
       });
   }, []);
+
+  // React.useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch('https://63fc3ef6859df29986b8ebfe.mockapi.io/items');
+  //       const data = await response.json();
+  //       if (data) {
+  //         setItems(data);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <div className="wrapper">
@@ -29,9 +50,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
