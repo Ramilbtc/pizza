2ef31dabea21d0db4e99e1d030c9b9ba8e168ef0
Ramil <105861531+Ramilbtc@users.noreply.github.com>
2023-02-27 09:00:45 +0300

    6.1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
-console.log(pizzas);
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   fetch('https://63fc3ef6859df29986b8ebfe.mockapi.io/items')
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((arr) => {
+  //       setItems(arr);
+  //     });
+  // }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -23,7 +31,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
