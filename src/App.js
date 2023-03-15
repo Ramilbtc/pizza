c978023fd89b5e97bba565122b7bf62d6f6d5c02
Ramil <105861531+Ramilbtc@users.noreply.github.com>
2023-03-15 21:04:21 +0300

    19.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
+import FullPizza from './pages/FullPizza';
 
 import './scss/app.scss';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">{children}</div>
+    </div>
+  );
+};
+
+export default MainLayout;
